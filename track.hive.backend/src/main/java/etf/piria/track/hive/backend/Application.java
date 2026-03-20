b5fe3d4253004e7d44bafd43d380878b4d5ef882
gprincip <tripic.zeljko@gmail.com>
2026-03-20 22:35:45 +0100

    Init

---
+++ track.hive.backend/src/main/java/etf/piria/track/hive/backend/Application.java
@@ -0,0 +1,13 @@
+package etf.piria.track.hive.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ track.hive.backend/src/test/java/etf/piria/track/hive/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package etf.piria.track.hive.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
