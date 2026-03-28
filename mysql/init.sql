0fb7db76dd00b64ff8a9e6fd58cbb8806ea7a1bb
gprincip <tripic.zeljko@gmail.com>
2026-03-28 21:41:20 +0100

    change init.sql with init.sh in order to read credentials from env

--- mysql/init.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER IF NOT EXISTS 'track-hive-backend'@'%' IDENTIFIED BY 'secret';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, INDEX, DROP ON `track-hive`.* TO 'track-hive-backend'@'%';
-FLUSH PRIVILEGES;
